420db11c595611a5d814f1db1608ebefc9393ee6
Valmir Medeiros Francisco <valmir.francisco@sonda.com>
2022-07-15 10:20:33 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,26 @@
+import axios from 'axios';
+import { BASE_URL } from "../../utils/request";
 import icon from '../../assets/img/notification-icon.svg';
 
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(saleId:number) {
+    axios.get(`${BASE_URL}/sales/${saleId}/notification`)
+    .then(response => {
+        console.log(response.data);
+        console.log(response.status);
+        console.log("Sucesso!");
+    })
+}
+
+function NotificationButton({saleId} : Props) {
     return (
 
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick({saleId}.saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
 
